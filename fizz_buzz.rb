21a0576414536258f60410a51389943aef847705
19951118 <reonn1118@yahoo.co.jp>
2023-12-23 05:25:48 +0000

    vscodeでfizzbuzz2

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz(num)
   if num % 15 == 0
     "fizzbuzz"
-  elsif num % 5 == 0
+  elsif num % 5 ==0
     "buzz"
   elsif num % 3 == 0
     "fizz"
@@ -10,7 +10,8 @@ def fizzbuzz(num)
   end
 end
 
-puts "数を入力"
-kazu = gets.to_i
-puts "結果↓"
-puts fizzbuzz(kazu)
+puts "整数を入力してください"
+sesu = gets.to_i
+
+puts "結果"
+puts fizzbuzz(sesu)
